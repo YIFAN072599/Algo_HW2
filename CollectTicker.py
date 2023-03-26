962f4cf345e382a97e9f2cc61dadcf266c5391dd
Ch1nZ <czhao0928@gmail.com>
2023-03-25 20:39:36 -0400

    Chen

--- CollectTicker.py
+++ CollectTicker.py
@@ -5,8 +5,8 @@ sp_path = 'data/s&p500.xlsx'
 
 def collect_ticker(path=sp_path):
     df = pd.read_excel(path, sheet_name="WRDS", engine="openpyxl")
-    df_Ticker = df["Trading Symbol"].unique()
-    return df_Ticker
+    df_ticker = df["Trading Symbol"].unique()
+    return df_ticker
 
 
 if __name__ == '__main__':
