846d5d0214347896e1c7f0525b8bb25368547977
Ch1nZ <czhao0928@gmail.com>
2023-03-27 15:13:40 -0400

    Chen

--- CollectTicker.py
+++ CollectTicker.py
@@ -3,15 +3,19 @@ import os
 import pandas as pd
 
 sp_path = 'data/s&p500.xlsx'
-src_dir = "C:\\Users\\Admin\\Downloads\\Algo HW2\\Algo HW2\\quotes\\quotes"
+src_dir = "/Users/chenzhao/Data/taq data/quotes"
+
 
 def collect_ticker(path=sp_path):
     df = pd.read_excel(path, sheet_name="WRDS", engine="openpyxl")
-    df_ticker = df["Trading Symbol"].unique()
+    df_ticker = list(df["Trading Symbol"].unique())
+    df_ticker.remove('ESRX')
     return df_ticker
 
+
 def collect_date(path=src_dir):
-    return os.listdir(src_dir)
+    return os.listdir(path)
+
 
 if __name__ == '__main__':
-    print(len(collect_date()))
+    print('ESRX' in collect_ticker())
